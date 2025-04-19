fab5552b04ef84db22ece42728aeb2ceaf2a2715
Mausam5055 <104557109+Mausam5055@users.noreply.github.com>
2025-04-20 03:22:36 +0530

    commit

--- src/components/NotSupported.tsx
+++ src/components/NotSupported.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 const NotSupported = () => {
   return (
     <div className="fixed space-y-3 top-0 left-0 flex flex-col h-screen w-screen items-center text-center justify-center bg-black text-white text-lg font-bold p-8">
-      Hi!
+      Hi priyanka here!
       <br />
       This viewport is not yet supported.<br></br> I'll be adding the support
       soon.
